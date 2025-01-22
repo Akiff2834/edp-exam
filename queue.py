7fb054743c6ee77a1afa698b85098c929643c476
Akif Sahin <151027912+Akiff2834@users.noreply.github.com>
2025-01-22 10:25:27 +0000

    Add class Applicant

---
+++ queue.py
@@ -0,0 +1,12 @@
+class CommunicationQueue:
+    def __init__(self):
+        self.queue = []
+
+    def publish(self, event):
+        self.queue.append(event)
+        print(f"Event published: {event.__class__.__name__} with payload: {event.payload}")
+
+    def process(self):
+        while self.queue:
+            event = self.queue.pop(0)
+            print(f"Processing event: {event.__class__.__name__} with payload: {event.payload}")
