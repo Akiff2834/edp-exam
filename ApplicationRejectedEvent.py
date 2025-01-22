3fd2fba18aea99969452ea3bec01bb164a532f25
Akif Sahin <151027912+Akiff2834@users.noreply.github.com>
2025-01-22 10:46:30 +0000

    Add class Applicant

---
+++ ApplicationRejectedEvent.py
@@ -0,0 +1,6 @@
+from Event import Event
+
+class ApplicationRejectedEvent(Event):
+    def __init__(self, application_name, job_title, reason):
+        payload = {"application_name": application_name, "job_title": job_title, "reason": reason}
+        super().__init__(payload)
