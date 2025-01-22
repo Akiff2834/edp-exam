187c4d938d876f8776434b6443ad3fb639bd0c71
Akif Sahin <151027912+Akiff2834@users.noreply.github.com>
2025-01-22 10:47:10 +0000

    Add class Applicant

---
+++ ApplicationSubmittedEvent.py
@@ -0,0 +1,6 @@
+from Event import Event
+
+class ApplicationSubmittedEvent(Event):
+    def __init__(self, application_name, job_title):
+        payload = {"application_name": application_name, "job_title": job_title}
+        super().__init__(payload)
