e8b6b30740814e044a5038e4bc3b5be90d73e5fd
Akif Sahin <151027912+Akiff2834@users.noreply.github.com>
2025-01-22 11:07:59 +0000

    Add class Applicant

--- applicant.py
+++ applicant.py
@@ -1,9 +1,7 @@
-from application_submitted_event import ApplicationSubmittedEvent # type: ignore
-
+from event import ApplicationSubmittedEvent
 class Applicant:
     def __init__(self, name):
         self.name = name
-
     def apply_for_job(self, job_title, company, queue):
         event = ApplicationSubmittedEvent(self.name, job_title)
         queue.publish(event)
