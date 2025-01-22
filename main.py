c6cba2f21a51f88f5e92bba45faa2fabb3e0e396
Akif Sahin <151027912+Akiff2834@users.noreply.github.com>
2025-01-22 10:07:36 +0000

    Add class Applicant

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ class Event:
     def __init__(self, payload):
         self.payload = payload
 
+from event import Event
+
 class ApplicationSubmittedEvent(Event):
     def __init__(self, application_name, job_title):
         payload = {"application_name": application_name, "job_title": job_title}
         super().__init__(payload)
+
+
+from event import Event
 
 class ApplicationRejectedEvent(Event):
     def __init__(self, application_name, job_title, reason):
@@ -25,6 +30,8 @@ class CommunicationQueue:
             event = self.queue.pop(0)
             print(f"Processing event: {event.__class__.__name__} with payload: {event.payload}")
 
+from application_submitted_event import ApplicationSubmittedEvent
+
 class Applicant:
     def __init__(self, name):
         self.name = name
@@ -32,6 +39,8 @@ class Applicant:
     def apply_for_job(self, job_title, company, queue):
         event = ApplicationSubmittedEvent(self.name, job_title)
         queue.publish(event)
+
+from application_rejected_event import ApplicationRejectedEvent
 
 class Company:
     def __init__(self, name):
@@ -41,17 +50,23 @@ class Company:
         event = ApplicationRejectedEvent(applicant_name, job_title, reason)
         queue.publish(event)
 
+
+ 
+
+from communication_queue import CommunicationQueue
+from applicant import Applicant
+from company import Company
+
 def main():
     queue = CommunicationQueue()
 
-    applicant = Applicant("Mehmet ")
+    applicant = Applicant("Mehmet")
     company = Company("Muzik company")
 
     applicant.apply_for_job("Software Engineer", company, queue)
 
-    
     company.reject_application(applicant.name, "Software Engineer", "Position filled", queue)
-    
+
     queue.process()
 
 if __name__ == "__main__":
