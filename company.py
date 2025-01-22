36ad7c5d1bf234ac1a0e729b1169894c00fd704d
Akif Sahin <151027912+Akiff2834@users.noreply.github.com>
2025-01-22 10:23:26 +0000

    Add class Applicant

---
+++ company.py
@@ -0,0 +1,9 @@
+from application_rejected_event import ApplicationRejectedEvent # type: ignore
+
+class Company:
+    def __init__(self, name):
+        self.name = name
+
+    def reject_application(self, applicant_name, job_title, reason, queue):
+        event = ApplicationRejectedEvent(applicant_name, job_title, reason)
+        queue.publish(event)
