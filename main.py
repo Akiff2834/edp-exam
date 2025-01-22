0c70005099b6268d0fad442c91b570e885af9313
Akif Sahin <151027912+Akiff2834@users.noreply.github.com>
2025-01-22 10:20:11 +0000

    Add class Applicant

--- main.py
+++ main.py
@@ -1,51 +1,6 @@
-class Event:
-    def __init__(self, payload):
-        self.payload = payload
-
-
-class ApplicationSubmittedEvent(Event):
-    def __init__(self, application_name, job_title):
-        payload = {"application_name": application_name, "job_title": job_title}
-        super().__init__(payload)
-
-
-class ApplicationRejectedEvent(Event):
-    def __init__(self, application_name, job_title, reason):
-        payload = {"application_name": application_name, "job_title": job_title, "reason": reason}
-        super().__init__(payload)
-
-
-class CommunicationQueue:
-    def __init__(self):
-        self.queue = []
-
-    def publish(self, event):
-        self.queue.append(event)
-        print(f"Event published: {event.__class__.__name__} with payload: {event.payload}")
-
-    def process(self):
-        while self.queue:
-            event = self.queue.pop(0)
-            print(f"Processing event: {event.__class__.__name__} with payload: {event.payload}")
-
-
-class Applicant:
-    def __init__(self, name):
-        self.name = name
-
-    def apply_for_job(self, job_title, company, queue):
-        event = ApplicationSubmittedEvent(self.name, job_title)
-        queue.publish(event)
-
-
-class Company:
-    def __init__(self, name):
-        self.name = name
-
-    def reject_application(self, applicant_name, job_title, reason, queue):
-        event = ApplicationRejectedEvent(applicant_name, job_title, reason)
-        queue.publish(event)
-
+from communication_queue import CommunicationQueue # type: ignore
+from applicant import Applicant
+from company import Company
 
 def main():
     queue = CommunicationQueue()
@@ -56,9 +11,8 @@ def main():
     applicant.apply_for_job("Software Engineer", company, queue)
 
     company.reject_application(applicant.name, "Software Engineer", "Position filled", queue)
-
+    
     queue.process()
-
 
 if __name__ == "__main__":
     main()
