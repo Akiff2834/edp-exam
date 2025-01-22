e9e83f4311b92ce30f2e7b0c3e088ea38b332390
Akif Sahin <151027912+Akiff2834@users.noreply.github.com>
2025-01-22 11:09:32 +0000

    Add class Applicant

--- event.py
+++ event.py
@@ -1,3 +1,11 @@
 class Event:
     def __init__(self, payload):
         self.payload = payload
+class ApplicationSubmittedEvent(Event):
+    def __init__(self, application_name, job_title):
+        payload = {"application_name": application_name, "job_title": job_title}
+        super().__init__(payload)
+class ApplicationRejectedEvent(Event):
+    def __init__(self, application_name, job_title, reason):
+        payload = {"application_name": application_name, "job_title": job_title, "reason": reason}
+        super().__init__(payload)
